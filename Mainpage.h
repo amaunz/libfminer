facf16ef89a1b113a8b7b3261283147cbe4940ec
Andreas Maunz <andreas@maunz.de>
2009-04-28 13:15:17 +0200

    Added custom Doxygen css and Clustermaps code

--- Mainpage.h
+++ Mainpage.h
@@ -213,4 +213,19 @@
  * Web: http://cs.maunz.de
  *
  *  \author © 2008 by Andreas Maunz, 2008
+ *
+ * \htmlonly
+ * <a href="http://www2.clustrmaps.com/counter/maps.php?url=http://www.maunz.de/libfminer-doc/main.html" id="clustrMapsLink"><img src="http://www2.clustrmaps.com/counter/index2.php?url=http://www.maunz.de/libfminer-doc/main.html" style="border:0px;visibility:hidden" alt="Locations of visitors to this page" title="Locations of visitors to this page" id="clustrMapsImg" />
+ * </a>
+ * <script type="text/javascript">
+ * function cantload() {
+ * img = document.getElementById("clustrMapsImg");
+ * img.onerror = null;
+ * img.src = "http://clustrmaps.com/images/clustrmaps-back-soon.jpg";
+ * document.getElementById("clustrMapsLink").href = "http://clustrmaps.com";
+ * }
+ * img = document.getElementById("clustrMapsImg");
+ * img.onerror = cantload;
+ * </script>
+ * \endhtmlonly
  **/
